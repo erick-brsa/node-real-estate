13de0b2d5ed8ed8d2816c672bebef0dc08ac8564
Erick Briones <erickalan518@gmail.com>
2022-12-06 22:41:02 -0600

    creating auth pages

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,17 @@
 export const loginForm = (req, res) => {
 	res.render('auth/login', {
-		auth: false
+		page: 'Iniciar Sesión'
 	});
 };
 
 export const registerForm = (req, res) => {
-    res.render('auth/register', {
-        auth: false
-    });
-}
+	res.render('auth/register', {
+		page: 'Crear Cuenta'
+	});
+};
+
+export const resetPassword = (req, res) => {
+	res.render('auth/reset-password', {
+		page: 'Recuperar Contraseña'
+	});
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { loginForm, registerForm } from '../controllers/userController.js';
+import {
+	loginForm,
+	registerForm,
+	resetPassword
+} from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/login', loginForm);
 router.get('/register', registerForm);
+router.get('/reset-password', resetPassword);
 
 export default router;
